0d37971b412805a9ee35977b44463012f7fa310d
yanuar-tess <yanuar@tessaract.io>
2024-02-21 04:51:35 +0700

    update the scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,14 @@
 # Install the application by modifying the supervisor configuration file
 echo "Installing the application"
-mv ephemeral_prometheus_metric /usr/local/bin
+if [ -z "$1" ]; then
+  pass
+  elif [ "$1" = "alpine" ]; then
+    echo "Copying the application to /usr/local/bin/ephemeral_prometheus_metric for alpine"
+    cp ./bin/alpine/ephemeral_prometheus_metric /usr/local/bin/ephemeral_prometheus_metric
+else
+  echo "Copying the application to /usr/local/bin/ephemeral_prometheus_metric for ubuntu"
+  cp ./bin/ubuntu/ephemeral_prometheus_metric /usr/local/bin/ephemeral_prometheus_metric
+fi
 
 # Modify the supervisord.conf file to include the application
 echo "Modifying the supervisord.conf file"
@@ -11,8 +19,3 @@ echo "command=/usr/local/bin/ephemeral_prometheus_metric" >> ../supervisord.conf
 echo "Reloading the supervisor configuration"
 supervisorctl -c "../supervisord.conf" reread
 supervisorctl -c "../supervisord.conf" update
-
-# Start the application
-cd ../
-echo "Starting the application"
-supervisorctl start ephemeral_prometheus_metric
